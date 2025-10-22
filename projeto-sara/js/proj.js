4f0f9622f4e37a9c09b18a87c2c72b13080e5203
SEU_NOME_DE_USUARIO <sara.y@escolar.ifrn.edu.br>
2025-10-22 01:06:17 -0300

    asdasdas

---
+++ projeto-sara/js/proj.js
@@ -0,0 +1,27 @@
+document.getElementById("btn1").addEventListener("click", function() {
+  window.location.href = "quemsomos.html";
+});
+
+document.getElementById("btn2").addEventListener("click", function() {
+  window.location.href = "nanotecnologia.html";
+});
+
+document.getElementById("btn3").addEventListener("click", function() {
+  window.location.href = "pesquisadores.html";
+});
+
+document.getElementById("btn4").addEventListener("click", function() {
+  window.location.href = "tipos.html";
+});
+
+document.getElementById("btnLogo").addEventListener("click", function() {
+  window.location.href = "proj.html";
+});
+
+document.getElementById("btn5").addEventListener("click", function() {
+  window.location.href = "biomaket.html";
+});
+
+document.getElementById("btn6").addEventListener("click", function() {
+  window.location.href = "nanotecnologia.html"; 
+});
